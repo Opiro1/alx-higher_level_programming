b4273ae1e76b8277c9a4e66259db84e7e2ab8423
Opiro1 <johnsonopeoluwa2304@gmail.com>
2023-04-25 09:23:29 -0700

    The web_scraping project

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -11,17 +11,4 @@ request.get(url, (error, response, body) => {
     const data = JSON.parse(body);
     console.log(data.title);
   }
-});#!/usr/bin/node
-
-const request = require('request');
-const movieId = process.argv[2];
-const url = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
-
-request.get(url, (error, response, body) => {
-  if (error) {
-    console.log(error);
-  } else {
-    const data = JSON.parse(body);
-    console.log(data.title);
-  }
 });
